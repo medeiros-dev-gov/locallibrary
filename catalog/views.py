64df19554a0cd59659f7cc47041343cfca81c7a5
Gabriel Medeiros <gabrielmedeirosbiel518@gmail.com>
2024-11-11 12:41:45 -0300

    parte 7 concluida(sessoes)

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views.generic import ListView, DetailView
 
 def index(request):
     """View function for home page of site."""
     # Generate counts of some of the main objects
-    num_books = Book.objects.all().count()
-    num_instances = BookInstance.objects.all().count()
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits + 1,
     }
 
     return render(request, 'index.html', context=context)
+
 
 class BookListView(ListView):
     model = Book
